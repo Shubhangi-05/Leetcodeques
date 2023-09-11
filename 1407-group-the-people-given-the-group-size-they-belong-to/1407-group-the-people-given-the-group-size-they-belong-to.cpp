971d909e3e4fd6bf2a9330058fa6dcfc78ee5378
Shubhangi-05 <91474175+Shubhangi-05@users.noreply.github.com>
2023-09-12 00:20:00 +0530

    Time: 7 ms (87.01%), Space: 12.5 MB (96.45%) - LeetHub

--- 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -5,18 +5,24 @@ public:
         unordered_map<int,vector<int>>mpp;
         for(int i=0;i<groupSizes.size();i++){
             mpp[groupSizes[i]].push_back(i);
-        }
-        for(int i=0;i<groupSizes.size();i++){
-            if(mpp[groupSizes[i]].size()>0){
-                int k=groupSizes[i];
-                vector<int>arr;
-                for(int j=0;j<k;j++){
-                    arr.push_back(mpp[k][j]);
-                }
-                ans.push_back(arr);
-                mpp[k].erase(mpp[k].begin(),mpp[k].begin()+k);
+            if(mpp[groupSizes[i]].size()==groupSizes[i]){
+                ans.push_back(mpp[groupSizes[i]]);
+                mpp[groupSizes[i]].clear();
             }
         }
+
+        //this is the long method
+        // for(int i=0;i<groupSizes.size();i++){
+        //     if(mpp[groupSizes[i]].size()>0){
+        //         int k=groupSizes[i];
+        //         vector<int>arr;
+        //         for(int j=0;j<k;j++){
+        //             arr.push_back(mpp[k][j]);
+        //         }
+        //         ans.push_back(arr);
+        //         mpp[k].erase(mpp[k].begin(),mpp[k].begin()+k);
+        //     }
+        // }
         return ans;
     }
 };
