a3972f315a8b94b8b794c409a3d63ddedca807ba
Shubhangi-05 <91474175+Shubhangi-05@users.noreply.github.com>
2023-09-12 19:24:24 +0530

    Time: 78 ms (37.14%), Space: 17.7 MB (14.93%) - LeetHub

--- 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -15,24 +15,42 @@ public:
 
         }
         int cnt=0;
-        sort(s.begin(),s.end());
-        for(int i=0;i<n;i++){
-            if(mpp.find(s[i])!=mpp.end()){
-                int x=mpp[s[i]];
-                if(x==0) continue;
-                if(mpp1[x]>1){
-                    // s.erase(s.begin()+i);
-                    cnt++;
-                    mpp1[x]--;
-                    mpp[s[i]]--;
-                    int l=mpp[s[i]];
-                    if(l>0){
-                        mpp1[l]++;
-                    }
-                }
+        // sort(s.begin(),s.end()); // I have to sort to make the same frequency characters to be together !
+        // for(int i=0;i<n;i++){
+        //     if(mpp.find(s[i])!=mpp.end()){
+        //         int x=mpp[s[i]];
+        //         if(x==0) continue;
+        //         if(mpp1[x]>1){
+        //             // s.erase(s.begin()+i);
+        //             cnt++;
+        //             mpp1[x]--;
+        //             mpp[s[i]]--;
+        //             int l=mpp[s[i]];
+        //             if(l>0){
+        //                 mpp1[l]++;
+        //             }
+        //         }
                
+        //     }
+        // }
+        for(auto i:mpp){
+            int k=i.second;
+            
+            if(mpp1[k]>1){
+                while(mpp1[k]>1){
+                    mpp1[k]--;
+                    cnt++;
+                    k--;
+                    if(k>0)
+                    mpp1[k]++;
+                    else break;
+                    
+                }
+                if(k>0){
+                    i.second=k;
+                }
             }
-        }
+        }  
         return cnt;
     }
 };
