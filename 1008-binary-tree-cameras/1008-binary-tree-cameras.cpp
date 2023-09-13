5067f03a3b23e1d4cd97f137747d0c829448a26b
Shubhangi-05 <91474175+Shubhangi-05@users.noreply.github.com>
2023-09-13 23:04:51 +0530

    Time: 8 ms (58.96%), Space: 21.5 MB (23.84%) - LeetHub

---
+++ 1008-binary-tree-cameras/1008-binary-tree-cameras.cpp
@@ -0,0 +1,77 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    // unordered_map<TreeNode*,TreeNode*>mpp;
+    // void par(TreeNode* root){
+    //     if(!root){
+    //         return;
+    //     }
+
+    //     if(root->left){
+    //         mpp[root->left]=root;
+    //         par(root->left);
+    //     }
+    //     if(root->right){
+    //         mpp[root->right]=root;
+    //         par(root->right);
+    //     }
+    // }
+    // int solve(TreeNode* root){
+    //     if(!root){
+    //         return 0;
+    //     }
+    //     int taken=1;
+    //     int nt_taken=0;
+    //     if(root->left){
+    //         taken+=solve(root->left->left)+solve(root->left->right);
+    //     }
+    //     if(root->right){
+    //         taken+=solve(root->right->left)+solve(root->right->right);
+    //     }
+    //     if(mpp.find(root)!=mpp.end()){
+    //         taken+=solve(mpp[root]->left)+solve(mpp[root]->right);
+    //     }
+    //     nt_taken=solve(root->left)+solve(root->right)+solve(mpp[root]);
+
+    //     return min(taken,nt_taken);
+    // }
+
+    int camera=0;
+    int solve(TreeNode* root){
+        if(!root){
+            return 1; //1-->covered
+        }
+        int lc=solve(root->left);
+        int rc=solve(root->right);
+        if(lc==-1 || rc==-1){ //-1-->there is a need of cameras 
+            camera++;
+            return 0;
+        }
+        if(lc==0|| rc==0){
+            return 1;  //child is covering me so i will return 1 stating that i am covered
+        }
+        return -1; //i need camera 
+
+    }
+    int minCameraCover(TreeNode* root) {
+        
+        if(!root){
+            return 0;
+        }
+        if(solve(root)==-1){
+            camera++;
+        }
+        return camera;
+    }
+};
